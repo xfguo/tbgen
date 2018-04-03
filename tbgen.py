73531b52a013ef8931bbf5a917b6c7c5971ead04
Xiongfei(Alex) GUO <xfguo@credosemi.com.cn>
2018-04-03 10:35:30 +0800

    update email.

--- tbgen.py
+++ tbgen.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
 # THE BEER-WARE LICENSE" (Revision 42):
-# <xfguo@credosemi.com> wrote this file. As long as you retain this notice you
+# <xfguo@xfguo.org> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return Xiongfei(Alex) Guo.
 
